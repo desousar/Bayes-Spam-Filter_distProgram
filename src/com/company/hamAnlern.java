c28af5be54a56a2a51edf61264d01733058b813b
Benoit De Sousa <benoit.sousa@hs.furtwangen.de>
2019-10-02 15:40:44 +0200

    premier commit

---
+++ src/com/company/hamAnlern.java
@@ -0,0 +1,72 @@
+package com.company;
+
+import java.io.*;
+import java.util.*;
+
+public class hamAnlern {
+
+    private HashMap<String, Integer> tableFinal = new HashMap<>(); //List von alle Wörter von alle Mail max: 2551-126 =2425
+
+    public HashMap<String, Integer> getTableFinal(){
+        return tableFinal;
+    }
+
+    public void anlern() throws IOException {
+
+
+        HashMap<String, Integer> tableEnCours = new HashMap<>(); //List von jede einzelne Mail mit Wörter max :1
+        String line;
+
+        File repertoire = new File("Programmieraufgabe1/ham-anlern");
+        String liste[] = repertoire.list();
+
+        for (String s : liste) {
+            System.out.println("une liste :" + s);
+
+            try {
+                BufferedReader in = new BufferedReader(new FileReader("Programmieraufgabe1/ham-anlern/" + s));
+
+                while ((line = in.readLine()) != null) {
+                    line = line.toLowerCase();
+                    StringTokenizer st = new StringTokenizer(line, " "); //split line
+                    while (st.hasMoreTokens()) {
+                        String str = st.nextToken();
+                        if (!tableEnCours.containsKey(str))
+                        {
+                            tableEnCours.put(str, 1);
+                        }
+                    }
+                }
+                in.close();
+
+                Set listkeys = tableFinal.keySet();
+                Iterator it = listkeys.iterator();
+                String str = " ";
+                while (it.hasNext()){
+                    Object key = it.next();
+                    if (tableFinal.containsKey(key)){
+                        tableFinal.put((String) key, tableFinal.get(key)+1);
+                    }
+                }
+
+                tableFinal.putAll(tableEnCours);
+                tableEnCours.clear();
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                e.printStackTrace();
+            }
+        }
+
+        BufferedWriter visualisation = new BufferedWriter(new FileWriter("ham.txt"));
+        Set listkeys = tableFinal.keySet();
+        Iterator it = listkeys.iterator();
+        String str = " ";
+        while (it.hasNext()){
+            Object key = it.next();
+            str = key + " " + tableFinal.get(key) + "\n";
+            visualisation.write(str);
+        }
+    }
+
+}
