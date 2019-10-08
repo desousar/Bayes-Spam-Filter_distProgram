8a8eb8962fd02638b8bca8d4ece1d928028f99ef
Benoit De Sousa <benoit.sousa@hs.furtwangen.de>
2019-10-08 12:33:52 +0200

    hamAnlern est totalement commenté en anglais

--- src/com/company/HamAnlern.java
+++ src/com/company/HamAnlern.java
@@ -1,26 +1,23 @@
 package com.company;
 
-import com.sun.security.auth.UnixNumericGroupPrincipal;
-
 import java.io.*;
 import java.util.*;
 
 public class HamAnlern {
 
-    private HashMap<String, Double> tableFinal = new HashMap<>(); //List von alle Wörter von alle Mail
+    private HashMap<String, Double> tableFinal = new HashMap<>(); //List with all words from all mail (ohne Mehrfachzählung in ein Mail)
 
     public HamAnlern() throws IOException {
 
         String line;
 
+        //obtain a list with all Files that contain our pathname
         File repertoire = new File("Programmieraufgabe1/ham-anlern");
         String[] liste = repertoire.list();
 
         assert liste != null;
         for (String s : liste) {
-            HashMap<String, Double> tableEnCours = new HashMap<>();//List von jede einzelne Mail mit Wörter max :1
-            //System.out.println("une liste :" + s);
-
+            HashMap<String, Double> tableEnCours = new HashMap<>();//List just for one mail
 
             try {
                 BufferedReader in = new BufferedReader(new FileReader("Programmieraufgabe1/ham-anlern/" + s));
@@ -31,17 +28,19 @@ public class HamAnlern {
                     while (st.hasMoreTokens()) {
                         String str = st.nextToken();
                         if (!tableEnCours.containsKey(str)) {
-                            tableEnCours.put(str, 1.0);
+                            tableEnCours.put(str, 1.0); //max rate apparition for one word = 1
                         }
 
                     }
                 }
                 in.close();
 
+                //convert the map tableEnCours to a set tableEnCours, so that we can work on it
                 Set<Map.Entry<String, Double>> setMap = tableEnCours.entrySet();
 
                 for (Iterator<Map.Entry<String, Double>> it = setMap.iterator(); it.hasNext(); ) {
                     Map.Entry<String, Double> map = it.next();
+                    //we put all tableEnCours in this map : tableFinal
                     if (tableFinal.containsKey(map.getKey())) {
                         tableFinal.put(map.getKey(), tableFinal.get(map.getKey()) + 1.0);
                     } else {
@@ -54,16 +53,12 @@ public class HamAnlern {
                 e.printStackTrace();
             }
         }
-
+        //to verify our code, we write the tableFinal in a txt => word + number of apparition in different mail
         this.write();
     }
 
-    /* sert uniquement au test :
-    < System.out.println("the ham-table " + h.getTableFinal(h)); //this is the correct map of all word from ham.anlern >
-    dans le main
-    */
+    // return our map tableFinal
     public HashMap<String, Double> getTableFinal() throws IOException {
-
         return tableFinal;
     }
 
@@ -77,6 +72,8 @@ public class HamAnlern {
         }
         return false;
     }
+
+    //return number of file in the pathname
     public int numberOfFileHamAnlern(){
         int number = 0;
         File repertoire = new File("Programmieraufgabe1/ham-anlern");
@@ -85,6 +82,7 @@ public class HamAnlern {
         return number;
     }
 
+    //return rate of apparition for one word in the tableFinal
     public double pourcentageForOneWordInHamAnlern(String word) throws IOException {
         int allFile =0;
         allFile = this.numberOfFileHamAnlern(); //number of file
@@ -92,9 +90,11 @@ public class HamAnlern {
         numberForWord = this.getTableFinal().get(word); //value of map from hamAnlern
         double result;
         result = numberForWord/allFile;
-        //System.out.println("the result is " + result);
         return result;
     }
+
+    /*If spamAnlern contain a word that hamAnlern doesn't contain, we add it with a rate of apparition very small :
+    it is for the mathematical formule*/
     public void addWordNotInSpamAnlern(SpamAnlern s) throws IOException{
         Set<Map.Entry<String, Double>> setMap = s.getTableFinal().entrySet();
         for (Map.Entry<String, Double> e : setMap) {
@@ -103,6 +103,8 @@ public class HamAnlern {
             }
         }
     }
+
+    //write the map in a txt, so that we can verify what there is inside (word and rate od apparition)
     public void write() throws IOException {
         BufferedWriter visualisation = new BufferedWriter(new FileWriter("ham-anlern.txt"));
         Set listkeys = tableFinal.keySet();
--- src/com/company/main.java
+++ src/com/company/main.java
@@ -21,42 +21,10 @@ public class main {
         hk.kallibrateMailToSet(kallibrateMail); //le paramètre ne permet pas d'écrire le set dans un doc txt
         /*   SpamKallibrate sk = new SpamKallibrate();
         System.out.println(sk.kallibrateMailToSet("00040.949a3d300eadb91d8745f1c1dab51133"));
-*/
-   //     System.out.println("the ham-table " + h.getTableFinal(h)); //this is the correct map of all word from ham.anlern
-   //     System.out.println("the hk-table " + hk.getTableFinal(hk, kallibrateMail)); //this is the correct set of the ham kallibrate that we test
-/*
-        //count how much word from hamKallibrate is also in hamAnlern
-        int countForHam = 0;
-            for (String wordKallibrate :hk.getTableFinal(hk,kallibrateMail)) {
-                if (h.containWord(wordKallibrate)) {
-                    countForHam++;
-                }else {
-                    System.out.println("non");
-                }
-            }
-        System.out.println("nombre total de correspondance avec Ham = " + countForHam);
+        */
 
-
-        //count how much word from hamKallibrate is also in spamAnlern
-        int countForSpam = 0;
-        for (String wordKallibrate :hk.getTableFinal(hk,kallibrateMail)) {
-            if (s.containWord(wordKallibrate)) {
-                countForSpam++;
-            }else {
-                System.out.println("non");
-            }
-        }
-        System.out.println("nombre total de correspondance avec Spam = " + countForSpam);
-
-        //print the result
-        if (countForHam > countForSpam){
-            System.out.println("c'est un ham car ham: "+ countForHam +" contre spam: "+countForSpam);
-        }else {
-            System.out.println("c'est un spam ham: "+ countForHam +" contre spam: "+countForSpam);
-        }
-*/
         System.out.println(h.numberOfFileHamAnlern() ); //Count the number of file Ham Anlern
-        System.out.println(h.pourcentageForOneWordInHamAnlern("frowning")+" ");// Give the procent of the word frowning
+        System.out.println("procent of the word frowning : "+ h.pourcentageForOneWordInHamAnlern("frowning"));// Give the procent of the word frowning
 
 
     }
